e74da46a789d40c18142b59961eb33e1ee186a99
Chris Moore <57775710+chrism-48@users.noreply.github.com>
2022-05-21 12:09:18 -0400

    added more assets

--- js/game.js
+++ js/game.js
@@ -1,14 +1,35 @@
 // create a new scene
 let gameScene = new Phaser.Scene('Game');
 
+
+// Load assets
+gameScene.preload = function() {
+    this.load.image('road', 'assets/background-1_2.png');
+    this.load.image('car', 'assets/car-top_view.png');
+
+};
+
+
+
+// Called once after preload ends
+gameScene.create = function() {
+    // Create bg sprite
+    this.add.image(318, 50, 'road');
+    this.add.image(380, 275, 'car');
+
+
+
+};
+
+
 // set the configuration of the game
 let config = {
     type: Phaser.AUTO,
     width: 640,
     height: 360,
     scene: gameScene
-
 };
+
 
 //create a new game, pass the configuration
 let game = new Phaser.Game(config);
